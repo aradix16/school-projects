6d48bf93eb37d8be208aafb02dd3be579145026a
Antoine Radix <aradix@student.42.fr>
2023-12-21 20:10:22 +0100

    (ft_printf): implement method to get printf flags on a single short type

--- ft_printf/srcs/parsing/get_flags.c
+++ ft_printf/srcs/parsing/get_flags.c
@@ -6,21 +6,39 @@
 /*   By: aradix <aradix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 19:23:16 by aradix            #+#    #+#             */
-/*   Updated: 2023/12/21 19:25:49 by aradix           ###   ########.fr       */
+/*   Updated: 2023/12/21 20:09:36 by aradix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_strchr(const char *s, const char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
 void	get_flags(t_printf *p)
 {
+	short	tmp;
 
+	while (1)
+	{
+		tmp = ft_strchr(" -+#0", *p->frmt);
+		if (tmp == -1)
+			break ;
+		p->flags |= (1 << tmp);
+		p->frmt++;
+	}
 
-
-	/* short	tmp; */
-	/**/
-	/* while ((tmp = s_chr(" -+#0", *p->frmt)) > -1 && *p->frmt++) */
-	/* 	p->flags |= (1 << tmp); */
 	/* get_width_pr(p); */
 	/* while ((tmp = s_chr("lh", *p->frmt)) > -1 && *p->frmt++) */
 	/* { */
@@ -39,5 +57,4 @@ void	get_flags(t_printf *p)
 	/* 	p->flags |= MINUS; */
 	/* } */
 	/**/
-
 }
